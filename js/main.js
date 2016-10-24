7818b008fc552c5ce0a308a0a988e6c3b9adae47
Juan Bonforti <juanbonforti@Juans-MacBook-Pro-2.local>
2016-10-24 15:15:16 -0300

    Eurolatino_Final_v1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-
+$(document).ready(function(){
+  //SmoothScrolling
+  $('nav li').on('click',function(){
+    $('nav ul').children('.active').removeClass();
+    var hash = $(this).children('a').attr('href');
+    $(this).addClass('active');
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 100
+    }, 'slow');    
+  });
+  //Navbar fixed
+  var headerSection = $('header'),
+  headerSectionOffset = headerSection.height()/2,
+  documentEl = $(document);
+  
+  documentEl.on('scroll', function(){
+    if (documentEl.scrollTop() > headerSectionOffset){
+      $('nav').addClass('navbar-default-scroll');
+      $('.navbar-collapse').children('.hidden').removeClass('hidden');
+    } else {
+      $('nav').removeClass('navbar-default-scroll');
+      $('.navbar-collapse').children('a').addClass('hidden');
+    }
+  });
+  // Slide menu function
   $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
-
   $('.info-viewer').on('click', function(e){
     if ($('.info').is(":hidden")){
       $('.info').stop(true, true).slideDown();
@@ -13,3 +36,5 @@
       $('.info').stop(true, true).slideUp();
     }    
   });
+});
+  
